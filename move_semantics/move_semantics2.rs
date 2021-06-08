592245d02fafd603046b92dc57c0d369cca4d9b4
Hui Ling <huilingw312@gmail.com>
2021-06-08 12:45:55 +0100

    completed all exercises on move semantics

--- move_semantics/move_semantics2.rs
+++ move_semantics/move_semantics2.rs
@@ -2,12 +2,19 @@
 // Make me compile without changing line 13!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    // Original Error message: is that a move of vec0 here and the subsequent
+    // computation of len in line 22 required a borrow
+    //
+    // I think because of fill_vec function, ownership of vec0 was transferred
+    // to vec1 (output of function) so vec0 is no longer in the scope. Hence,
+    // it does not exist anymore. So when it is being called in line 22, the
+    // length can no longer be found. Hence, throwing an error.
+    // Therefore, to prevent it from leaving the scope, a clone of vec0 needs
+    // to be passed to the function such that vec0 still persists in main
+    let mut vec1 = fill_vec(vec0.clone());
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
--- move_semantics/move_semantics3.rs
+++ move_semantics/move_semantics3.rs
@@ -3,8 +3,6 @@
 // (no lines with multiple semicolons necessary!)
 // Execute `rustlings hint move_semantics3` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
@@ -17,7 +15,10 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+// Soln: Make vec mutable in the function declaration
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
+    // Original Error Message: can't perform any of these pushes as vec
+    // is not mutable
     vec.push(22);
     vec.push(44);
     vec.push(66);
